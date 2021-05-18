2675261ba4af7b98223bea4650f1b0324ba332a3
Flammable-Duck <flammableduck@protonmail.com>
2021-05-18 09:40:14 -0700

    dictionary works ig aa

--- src/color.nim
+++ src/color.nim
@@ -1,9 +1,17 @@
 import tables
 
-const colors = to_ordered_table({"black": "\u001b[30m", "red": "\u001b[31m", "green": "\u001b[32m", "yellow": "\u001b[33m", "blue": "\u001b[34m", "magenta": "\u001b[35m", "cyan": "\u001b[36m", "white": "\u001b[37m", "reset": "\u001b[0m", })
+const colors = to_ordered_table({"black": "\u001b[30m",
+                                "red": "\u001b[31m",
+                                "green": "\u001b[32m",
+                                "yellow": "\u001b[33m",
+                                "blue": "\u001b[34m",
+                                "magenta": "\u001b[35m",
+                                "cyan": "\u001b[36m",
+                                "white": "\u001b[37m",
+                                "reset": "\u001b[0m", })
 
 proc main() =
-    for value, key in colors:
+    for key, value in colors:
         echo value, key
     
 when isMainModule:
