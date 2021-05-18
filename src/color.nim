63bb36030f383063b85450ff53c5fde6d69eeda2
Flammable-Duck <flammableduck@protonmail.com>
2021-05-18 01:35:30 -0700

    turned values into a table (nim's eqivelant to a dictionary)

--- src/color.nim
+++ src/color.nim
@@ -1,36 +1,10 @@
-type termColors = object
-    black: string
-    red: string
-    green: string
-    yellow: string
-    blue: string
-    magenta: string
-    cyan: string
-    white: string
-    reset: string
+import tables
 
-func Colors(): termColors =
-    result.black = "\u001b[30m"
-    result.red = "\u001b[31m"
-    result.green = "\u001b[32m"
-    result.yellow = "\u001b[33m"
-    result.blue = "\u001b[34m"
-    result.magenta = "\u001b[35m"
-    result.cyan = "\u001b[36m"
-    result.white = "\u001b[37m"
-    result.reset = "\u001b[0m"
+const colors = to_ordered_table({"black": "\u001b[30m", "red": "\u001b[31m", "green": "\u001b[32m", "yellow": "\u001b[33m", "blue": "\u001b[34m", "magenta": "\u001b[35m", "cyan": "\u001b[36m", "white": "\u001b[37m", "reset": "\u001b[0m", })
 
 proc main() =
-    echo(Colors().black, "test")
-    echo(Colors().red, "test")
-    echo(Colors().green, "test")
-    echo(Colors().yellow, "test")
-    echo(Colors().blue, "test")
-    echo(Colors().magenta, "test")
-    echo(Colors().cyan, "test")
-    echo(Colors().white, "test")
-    echo(Colors().reset, "test")
-    
+    for value, key in colors:
+        echo value, key
     
 when isMainModule:
   main()
